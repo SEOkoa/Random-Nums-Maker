ee3790814588068b9d3b9f611ed2065cb0816857
SEOkoa <kaze11koa@gmail.com>
2021-09-19 13:03:20 +0900

    Initial Commit

---
+++ Random Nums Maker/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Random Nums Maker
+//
+//  Created by SEO.koa on 2021/09/19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
