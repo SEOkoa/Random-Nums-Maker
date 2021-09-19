2a88525459cff3217fb12ecfd4778e48e9358c0b
SEOkoa <kaze11koa@gmail.com>
2021-09-19 15:40:34 +0900

    정렬 프로그램 만드는데 난수 입력하는게 귀찮아서 만들었습니다. 중복되지 않게 1 ~ 99 사이의 값을 1 ~ 99 개 만큼 만들 수 있게 했는데 ,앞으로 몇몇 개선은 있을 예정입니다.

--- Random Nums Maker/main.cpp
+++ Random Nums Maker/main.cpp
@@ -6,9 +6,52 @@
 //
 
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    srand(time(NULL));
+    
+    int knd = 0;
+    int array[100] = {0};
+    
+    cout << "1 ~ 99 사이의 '중복되지 않는' 난수를 생성합니다." <<endl;
+    cout << "몇 개의 난수를 생성하시겠습니까? : ";
+    scanf (" %d", &knd);
+    
+    for (int i = 0; i < knd; i++) {
+        array[i] = rand() % 100;
+        
+        if( array[i] == 0) {
+            i--;
+            cout << "0이 입력되어 i 재시도" <<endl;
+            continue;
+        }
+        
+        for (int j = 0; j < i - 1; j++) {
+            
+            if ( array[i] == array[j] ) {
+                cout << "비교중인 값 " << array[i] << " 배열 속 값(위치:  "  << i << " )" << array[j] <<" 중복 숫자 발견 " <<endl;
+                i--;
+                j--;
+            }
+               
+        }
+    }
+    
+    for (int i = 0; i < knd; i++) {
+        cout << array[i] << " ";
+    }
+    
+    cout << "0";
+    cout << endl;
+
+
+
     return 0;
 }
